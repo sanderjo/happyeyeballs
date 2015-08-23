da6ae5f01356258225bfe525d5c8ae8feccc23d3
sanderjo <sander.jonkers+github@gmail.com>
2015-08-23 23:17:47 +0200

    Update happyeyeballs-multiprocessing.py

--- happyeyeballs-multiprocessing.py
+++ happyeyeballs-multiprocessing.py
@@ -1,4 +1,6 @@
 # Multiprocessing module for Happy Eyeballs
+# Method on high level: start a Process for each IP address (adding a delay for IPv4 addresses), connect & 
+# find the quickest response, report that and terminate() all other processes that might still be running.
 
 import time, socket
 from multiprocessing import Process, Queue
